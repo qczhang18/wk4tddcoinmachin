4caa16d06ada03b5ee6ee602074e21c7fa7e5083
qczhang18 <qczhang18@gmail.com>
2018-03-20 11:55:20 -0400

    init and primed

---
+++ src/main/java/com/digitalcrafts/syntel/wk4tddcoinmachine/Wk4tddcoinmachineApplication.java
@@ -0,0 +1,12 @@
+package com.digitalcrafts.syntel.wk4tddcoinmachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wk4tddcoinmachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wk4tddcoinmachineApplication.class, args);
+	}
+}
---
+++ src/test/java/com/digitalcrafts/syntel/wk4tddcoinmachine/Wk4tddcoinmachineApplicationTests.java
@@ -0,0 +1,16 @@
+package com.digitalcrafts.syntel.wk4tddcoinmachine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Wk4tddcoinmachineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
